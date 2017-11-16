28dad0b8b32f732368ecc52c0aa256d37fc58855
sumeghaGIT <sumegha.razdan@esoftech.com>
2017-11-16 11:48:47 +0530

    updated changes

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models
-# from django.contrib.auth.models import User as AuthUser
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 
