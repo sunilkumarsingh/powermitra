b5f374efb104b2d9d9d8bf1e5624150348b05dff
sumeghaGIT <sumegha.razdan@esoftech.com>
2017-11-16 11:45:58 +0530

    updated models of users

--- users/models.py
+++ users/models.py
@@ -2,12 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import User as AuthUser
+# from django.contrib.auth.models import User as AuthUser
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
 class UserType(models.Model):
-    # user_type = models.CharField(unique=True, max_length=2, blank=True, null=True)
     user_type = models.CharField(unique=True, max_length=64, blank=True, null=True)
     is_active = models.CharField(max_length=1, blank=True, null=True)
     created_by = models.CharField(max_length=64, blank=True, null=True)
@@ -16,23 +17,24 @@ class UserType(models.Model):
     update_date = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        # managed = False
         db_table = 'pm_user_type'
 
         def __str__(self):
             return self.name
 
 
-class User(models.Model):
-    login_id = models.CharField(unique=True, max_length=64, blank=True, null=True)
-    is_active = models.CharField(max_length=1, blank=True, null=True)
+class User(AbstractUser):
     user_type = models.ForeignKey('UserType', models.DO_NOTHING)
-    auth_user_id = models.OneToOneField(AuthUser, models.DO_NOTHING, db_column="auth_user_id", related_name='pmUser')
+    phone_regex = RegexValidator(regex='/^(\+\d{1,3}[- ]?)?\d{10}$/',
+                                 message="Mobile number must be entered in the format: '+999999999'. Up to 10 digits allowed.")
+    mobiles = models.CharField(max_length=10, validators=[phone_regex], blank=True)
+    city = models.CharField(max_length=200, blank=True, null=True)
+    state = models.CharField(max_length=200, blank=True, null=True)
+    # auth_user_id = models.OneToOneField(AuthUser, models.DO_NOTHING, db_column="auth_user_id", related_name='pmUser')
     created_by = models.CharField(max_length=64, blank=True, null=True)
     create_date = models.DateTimeField(blank=True, null=True)
     updated_by = models.CharField(max_length=64, blank=True, null=True)
     update_date = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        # managed = False
         db_table = 'pm_user'
